d4b0c1182d50cb114ed39bf08208f87529f5ffe7
Günsu Günaydın <110038957+gunsugunaydin@users.noreply.github.com>
2024-07-06 03:30:14 +0300

    Add files via upload

---
+++ src/main/java/org/twids/SpringBlog/repositories/AccountRepository.java
@@ -0,0 +1,16 @@
+package org.twids.SpringBlog.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.twids.SpringBlog.models.Account;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long>{
+    
+    //JPARepoda olmayan methodları ekliyoruz.
+    Optional<Account> findOneByEmailIgnoreCase(String email);
+    Optional<Account> findByToken(String token);
+    
+}
